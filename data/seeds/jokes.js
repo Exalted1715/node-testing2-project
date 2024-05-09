179daba5dbc8c8bcd858bcb703e2330c6b489d3c
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-05-09 12:37:00 -0600

    wip

--- data/seeds/jokes.js
+++ data/seeds/jokes.js
@@ -1,3 +1,11 @@
 exports.seed = function(knex){
-    
-}
+    return knex('jokes').truncate()
+        .then(function () {
+            return knex('jokes').insert([
+                {joke: "what did the tin man say when he got run over by a steamroller?", punchline: "curses, foiled again!"},
+                {joke: "how do you make a tissue dance?", punchline: " you put a little boogey in it!"},
+                {joke: "whats an astronauts favorite part of a computer?", punchline: "the spacebar"},
+                {joke:"what do you get from a pampered cow?", punchline: "spoiled milk"}
+            ]);
+        });
+};
